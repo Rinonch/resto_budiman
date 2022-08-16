82612ff26d0a7cb6fd76439679c9f0478aebc3f2
Hestarus <deafendttack38@gmail.com>
2022-08-16 07:59:10 +0700

    add image

---
+++ resources/views/index.blade.php
@@ -0,0 +1,83 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="/css/index.css">
+    <title>Resto Budiman | Home</title>
+</head>
+
+<body>
+    <div class="wrapper">
+        <img src="/assets/web-banner.png" style="width: 100%;">
+        <header>
+            <nav>
+                <ul>
+                    <li><a href="/" class="beranda">Beranda</a></li>
+                    <li><a href="pesan.html" target="_blank">Pesan</a></li>
+                    <li><a href="daftar-page.html" target="_blank">Daftar</a></li>
+                    <li><a href="masuk_page.html" target="_blank">Masuk</a></li>
+                </ul>
+            </nav>
+        </header>
+
+        <section class="menu">
+            <article>
+                <figure>
+                    <img src="/assets/ayamgeprek.jpg" style="height: 200px; width: 245px;">
+                    <figcaption>Ayam Geprek</figcaption>
+                </figure>
+                <hgroup>
+                    <h2>Ayam Geprek</h2>
+                    <h3>Makanan Pedas</h3>
+                </hgroup>
+                <p>
+                    Ayam geprek adalah makanan ayam goreng tepung khas Indonesia yang diulek bersama sambal bawang.
+                    Kini ayam geprek telah menjadi hidangan populer yang dapat ditemukan di hampir semua kota besar di
+                    Indonesia,
+                    tetapi asal mula ayam geprek berasal dari kota Yogyakarta.
+                </p>
+            </article>
+            <article>
+                <figure>
+                    <img src="/assets/ayamgoreng.jpg" style="height: 200px; width: 245px;">
+                    <figcaption>Ayam Goreng</figcaption>
+                </figure>
+                <hgroup>
+                    <h2>Ayam Goreng</h2>
+                    <h3>Makanan Khas Indonesia</h3>
+                </hgroup>
+                <p>
+                    Ayam goreng Nusantara adalah hidangan Asia Tenggara yang merupakan ayam yang digoreng dalam minyak
+                    goreng.
+                    Dalam dunia internasional, istilah ayam goreng merujuk kepada ayam goreng gaya Nusantara.
+                </p>
+            </article>
+        </section>
+        <aside>
+            <section class="menu-populer">
+                <h2>Menu lainnya :</h2>
+                <a href="menu1-2.html">Ayam Bakar </a>
+                <a href="menu1-2.html">Ayam Penyet</a>
+                <a href="menu3-4.html">Lalapan</a>
+                <a href="menu3-4.html">Sambel Matah</a>
+            </section>
+            <section class="kontak">
+                <h2>Kontak :</h2>
+                <img src="/assets/logo-ig.png" style="width: 35px; height: 35px;"> <br> @resto_budiman01
+                <br>
+                <img src="/assets/logo-wa.png" style="width: 35x; height: 35px;"> <br> 08777467884 (Jakarta) <br> 08952534251
+                (Bandung) <br> 08223387654 (Semarang)
+                <br>
+                <img src="/assets/logo-fb.png" style="width: 35px; height: 35px;"> <br> Resto Budiman
+            </section>
+        </aside>
+        <footer>
+            Copyright &copy; Resto Budiman 2021
+        </footer>
+    </div>
+</body>
+
+</html>
